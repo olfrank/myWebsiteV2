dca16789a35ac161b90ef8e7d915815dcf014975
Oliver Francis <olliefrancis97@hotmail.co.uk>
2021-09-27 15:06:26 +0100

    emailjs implementation

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -1,9 +1,9 @@
-import React, { useState} from "react";
+import React from "react";
 import '../pages/contact.css';
 import Video from "../video/StarsBg.mp4";
 import Footer from '../components/Footer';
 import '../App.css';
-
+import emailjs from 'emailjs-com'
 import { Header, 
          HeroBg,
          VideoBg, 
@@ -22,30 +22,50 @@ import { Header,
 
 
 const ContactForm = () =>{
-    const [status, setStatus] = useState("Submit");
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setStatus("Sending...");
-        const { name, email, subject, message } = e.target.elements;
-        let details = {
-            name: name.value,
-            email: email.value,
-            subject: subject.value,
-            message: message.value,
-        };
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(details),
-        });
-        setStatus("Submit");
-        let result = await response.json();
-        alert(result.status)
-        
-        
-    };
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_asvuevp', 'template_nuo5s7p', e.target, 'user_APYoiu6sPj8GgJP8L0Hgw')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      alert("Your Message has been sent!");
+      e.target.reset();
+}
+
+
+
+
+
+
+
+  /******** Node.js email server (Github Pages does not support server side technology so i opted for EmailJS instead)******** */
+    // const [status, setStatus] = useState("Submit");
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     setStatus("Sending...");
+    //     const { name, email, subject, message } = e.target.elements;
+    //     let details = {
+    //         name: name.value,
+    //         email: email.value,
+    //         subject: subject.value,
+    //         message: message.value,
+    //     };
+    //     let response = await fetch("http://localhost:5000/contact", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json;charset=utf-8",
+    //         },
+    //         body: JSON.stringify(details),
+    //     });
+    //     setStatus("Submit");
+    //     let result = await response.json();
+    //     alert(result.status)
+    // };
+
+
 
     
     return (
@@ -61,25 +81,25 @@ const ContactForm = () =>{
             </HeroBg>
           <div className="container">
             
-            <form className="form" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={sendEmail}>
             <MailIcon/>
               <NameField>
                 <label htmlFor="name">name: </label>
-                <input type="text" id="name"  required className="input-fields"></input>
+                <input type="text" id="name"  required className="input-fields" name="name"></input>
               </NameField>
               <EmailField>
                 <label htmlFor="email">email: </label>
-                <input type="email" id="email" required className="input-fields"/>
+                <input type="email" id="email" required className="input-fields" name="email"/>
               </EmailField>
               <SubjectField>
                 <label htmlFor="subject">subject: </label>
-                <input type="subject" id="subject" required className="input-fields"/>
+                <input type="text" id="subject" required className="input-fields" name="subject"/>
               </SubjectField>
               <MessageField>
                 <label htmlFor="message">message: </label>
-                <textarea id="message" required className="text-fields"/>
+                <textarea id="message" required className="text-fields" name="message"/>
               </MessageField>
-              <Button  type="submit">{status}</Button>
+              <Button  type="submit" value="send message">submit</Button>
               <ResetBtn  type="reset" ><ResetArrow/></ResetBtn>
             </form>
           </div>
